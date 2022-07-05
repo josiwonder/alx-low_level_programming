3217ad06dc2eeb9a034bf51b1eba367fc33e4df1
Omoba Josiah <josiwonder@gmail.com>
2022-07-04 21:43:47 -0700

    coding scripts

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+nclude <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ *main - Entry point
+ *Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < = 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num = = 9)
+			continue;
+		putchar(',');
+		putchar('!');
+	}
+	putchar('\n');
+	return (0);
+}
