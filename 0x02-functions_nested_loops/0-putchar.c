68d6e84401d668ec4cde432965ffbb33b4e244f7
Omoba Josiah <josiwonder@gmail.com>
2022-07-05 18:35:52 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _putchar( );
+
+int main(void)
+{
+   _putchar();
+   return 0;
+}
+
+int _putchar( )
+{
+printf ("_purtchar \n");
+return 0;
+}
