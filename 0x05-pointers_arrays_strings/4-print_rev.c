58eacf1941a4ee643c8f48fdd7745b5c095dc871
Omoba Josiah <josiwonder@gmail.com>
2022-07-12 22:10:44 -0700

    code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
-*print_rev - prints reversed string, followed by a new line 
+*print_rev - prints reversed string, followed by a new line
 *@s: pointer to the string to print * Return: void
 */
 void print_rev(char *s)
 {
 	int i = 0;
 	while (s[i])
-		i++; 
-	
+
+		i++;	
 	while (i--)
-	{	
+	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
