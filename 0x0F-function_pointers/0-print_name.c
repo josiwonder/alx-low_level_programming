5a6eabace15f559c33048bca73fe42e6e15d35a7
Omoba Josiah <josiwonder@gmail.com>
2022-08-05 21:20:18 -0700

    coding scripts

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,8 +5,9 @@
  * @name: char string
  * @f: function pointer that takes a string argument
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
 }
